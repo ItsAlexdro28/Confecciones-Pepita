39d86b1c543810bc6bd82b34a505d8812a6aed85
ItsAlexdro28 <alejandroj5025@gmail.com>
2024-05-29 21:57:19 -0500

    feat: :sparkles: Added get data from API function

---
+++ src/calculos/get-data.js
@@ -0,0 +1,21 @@
+export const getApiData = async (endpoint = '') => {
+    const url = `https://6657c5835c3617052645d233.mockapi.io/${endpoint}`;
+    try {
+        const response = await fetch(url, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        if (!response.ok) {
+            throw new Error('Error al obtener datos del MockAPI');
+        }
+
+        const responseData = await response.json();
+        console.log('Datos obtenidos de la API:', responseData);
+        return responseData; // Export or use the data as needed
+    } catch (error) {
+        console.error('Error al obtener datos de la API:', error);
+    }
+}
