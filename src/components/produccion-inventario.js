539d99c44983f5413892a4618555827c75f19db1
MariaFernandaGamez <marfgamez04@gmail.com>
2024-06-04 13:29:06 -0500

    :lipstick: Inventario estilos

--- src/components/produccion-inventario.js
+++ src/components/produccion-inventario.js
@@ -4,86 +4,254 @@ export const productos = [
     {
         imagen: ('../public/images/pantalon1.webp'),
         nombre: "Pantalón elegante mujer",
-        tallas:"s - m - l",
-        metrosMateria: "5",
-        botones:"2"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "5",
+                botones:"2"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "7",
+                botones:"2"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "9",
+                botones:"2"
+            }
+        }
     },
     {
         imagen: ('../public/images/blusa1.webp'),
         nombre: "Blusa elegante mujer",
-        tallas:"s - m - l",
-        metrosMateria: "2",
-        botones:"1"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "4",
+                botones:"2"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "6",
+                botones:"2"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "8",
+                botones:"2"
+            }
+        }
     },
     {
         imagen: ('../public/images/chaleco1.jpg'),
         nombre: "Chaleco traje hombre",
-        tallas:"34 - 36 - 38",
-        metrosMateria: "4",
-        botones:"5"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "9",
+                botones:"8"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "10",
+                botones:"8"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "12",
+                botones:"8"
+            }
+        }
     },
     {
         imagen: ('../public/images/gala1.jpg'),
         nombre: "Vestido gala mujer",
-        tallas:"xs - s - m",
-        metrosMateria: "7",
-        botones:"4"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "14",
+                botones:"4"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "16",
+                botones:"2"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "18",
+                botones:"4"
+            }
+        }
     },
     {
         imagen: ('../public/images/gala2.jpg'),
         nombre: "Vestido formal mujer",
-        tallas:"xs - s - m",
-        metrosMateria: "8",
-        botones:"1"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "17",
+                botones:"3"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "19",
+                botones:"3"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "21",
+                botones:"3"
+            }
+        }
     },
     {
         imagen: ('../public/images/traje1.jpg'),
         nombre: "Traje boda hombre",
-        tallas:"m - l- xl",
-        metrosMateria: "12",
-        botones:"8"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "30",
+                botones:"3"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "34",
+                botones:"3"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "38",
+                botones:"3"
+            }
+        }
     },
     {
         imagen: ('../public/images/traje2.jpg'),
         nombre: "Traje formal hombre",
-        tallas:"m - l - xl",
-        metrosMateria: "10",
-        botones:"9"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "31",
+                botones:"9"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "33",
+                botones:"9"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "35",
+                botones:"9"
+            }
+        }
     },
     {
         imagen: ('../public/images/pantalon1.webp'),
         nombre: "Pantalón elegante mujer",
-        tallas:"s - m - l",
-        metrosMateria: "5",
-        botones:"2"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "13",
+                botones:"1"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "14",
+                botones:"1"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "15",
+                botones:"1"
+            }
+        }
     },
     {
         imagen: ('../public/images/vestido1.webp'),
         nombre: "Vestido de novia",
-        tallas:"s - m - l",
-        metrosMateria: "20",
-        botones:"5"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "40",
+                botones:"5"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "44",
+                botones:"5"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "46",
+                botones:"5"
+            }
+        }
     },
     {
         imagen: ('../public/images/vestido2.webp'),
         nombre: "Vestido de novia",
-        tallas:"s - m - l",
-        metrosMateria: "18",
-        botones:"2"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "45",
+                botones:"5"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "48",
+                botones:"5"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "51",
+                botones:"5"
+            }
+        }
     },
     {
         imagen: ('../public/images/playera.webp'),
         nombre: "Playera unicolor hombre",
-        tallas:"s - m - l",
-        metrosMateria: "6",
-        botones:"4"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "10",
+                botones:"3"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "12",
+                botones:"3"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "14",
+                botones:"3"
+            }
+        }
     },
     {
         imagen: ('../public/images/cuello.jpg'),
         nombre: "Camisa elegante hombre",
-        tallas:"s - m - l",
-        metrosMateria: "5",
-        botones:"0"
+        material: {
+            s: {
+                talla: "S",
+                metrosMateria: "9",
+                botones:"0"
+            },
+            m: {
+                talla: "M",
+                metrosMateria: "11",
+                botones:"0"
+            },
+            l: {
+                talla: "L",
+                metrosMateria: "12",
+                botones:"0"
+            }
+        }
     }
 ]
 
@@ -110,9 +278,25 @@ class ProductosComponent extends LitElement {
                         <img src="${producto.imagen}" alt="${producto.nombre}">
                         <div class="info">
                             <h3>${producto.nombre}</h3>
-                            <p>Tallas: ${producto.tallas}</p>
-                            <p>Metros de material: ${producto.metrosMateria}</p>
-                            <p>Botones: ${producto.botones}</p>
+                            <div class="tallasTabla">
+                                <div class="tallaInfo">
+                                    <p>Talla: ${producto.material.s.talla}</p>
+                                    <p>Metros: ${producto.material.s.metrosMateria}</p>
+                                    <p>Botones: ${producto.material.s.botones}</p>
+                                </div>
+
+                                <div class="tallaInfo">
+                                    <p>Talla: ${producto.material.m.talla}</p>
+                                    <p>Metros: ${producto.material.m.metrosMateria}</p>
+                                    <p>Botones: ${producto.material.m.botones}</p>
+                                </div>
+
+                                <div class="tallaInfo">
+                                    <p>Talla: ${producto.material.l.talla}</p>
+                                    <p>Metros: ${producto.material.l.metrosMateria}</p>
+                                    <p>Botones: ${producto.material.l.botones}</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 `)}
